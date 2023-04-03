51bddb40d2a4a701a287b65431c3827d90470c29
zehraghali <zehraghali@gmail.com>
2023-04-03 22:53:58 +0300

    teht 1

--- 13.1.py
+++ 13.1.py
@@ -1,21 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, Reponse
+import json
 
 app = Flask(__name__)
+@app.route('/alkuluku/<luku>')
+def on_alkuluku(luku):
+    luku = int(luku)
+    alkuluku = True
 
-def is_prime(n):
-    """Tarkistaa, onko annettu luku alkuluku."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+    for i in range(2, luku, 1):
+        if luku & i == 0:
+            alkuluku = False
+            break
 
-@app.route('/alkuluku/<int:n>')
-def check_prime(n):
-    """Palauttaa True, jos annettu luku on alkuluku, muuten False."""
-    result = {"Number": n, "isPrime": is_prime(n)}
-    return jsonify(result)
+    vastaus = {
+        "Number": luku,
+        "isPrime": alkuluku
+
+    }
+
+    jsonvast = json.dumps(vastaus)
+    return Reponse (reponse=jsonvast, mimetype="application/json")
+
+@app.errorhandler(404)
+def page_not_found(virhekoodi):
+    vastaus = {
+        "status": "404",
+        "teksti": "Virheellinen päätepiste"
+
+    }
+    jsonvast = json.dumps(vastaus)
+    return Reponse(response=jsonvast, status=404, mimetype="application/json")
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
+
+
+
